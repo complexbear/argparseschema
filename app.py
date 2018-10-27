8552edddea1330e3178b90d427e804d21ae81887
complexbear <matthewpeterbrown@gmail.com>
2018-10-27 15:31:32 +0100

    Demo service

---
+++ app.py
@@ -0,0 +1,33 @@
+import argparse
+
+from flask import request, Flask, jsonify
+from flask_apispec import FlaskApiSpec, use_kwargs
+
+from argparseschema import parser_to_schema
+
+
+def main():
+    app = Flask('MySchemaApp')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-x', dest='xvalue', type=str, help='the x value', default='hello')
+    parser.add_argument('-y', dest='yvalue', type=int, help='the y value', default=10)
+
+    MySchema = parser_to_schema(parser, 'MySchema')
+    my_schema = MySchema()
+
+    @app.route('/api')
+    @use_kwargs(MySchema, locations=['query'])
+    def handler(**kwargs):
+        obj = my_schema.load(kwargs)
+        print('recv args: {}'.format(kwargs))
+        print('recv obj: {}'.format(obj))
+        return my_schema.dump(obj)
+
+    docs = FlaskApiSpec(app)
+    docs.register(handler)
+    return app
+
+if __name__ == '__main__':
+    app = main()
+    app.run(debug=True, port=8080)
